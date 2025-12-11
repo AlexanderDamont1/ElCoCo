f0fd75d8ba041d7e99db904c63162c1cd7b56bc0
Alexander S <alexanderevobiketi@gmail.com>
2025-12-11 10:19:05 -0600

    Navegador de admin

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,7 +21,6 @@
                     <x-application-logo class="block h-12 w-auto fill-current text-gray-800" />
                 </a>
             </div>
-
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
